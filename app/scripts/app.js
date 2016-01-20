d3d86f0ca6149538707926a704a254f38ee3eacc
perushinkov <fantasmo3@gmail.com>
2016-01-20 18:15:51 +0200

    boilerplate

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularDhtmlxDemoApp
+ * @description
+ * # angularDhtmlxDemoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularDhtmlxDemoApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularDhtmlxDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularDhtmlxDemoApp
+ */
+angular.module('angularDhtmlxDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
